e6fcf0b62ff832d6f414af3906d41989b26b6f9e
laxeder <laxederbr@gmail.com>
2022-12-13 15:35:08 -0300

    Add ListAccount

---
+++ pkg/routes/account/ListAccount.go
@@ -0,0 +1,22 @@
+package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/laxeder/go-shop-service/pkg/modules/account"
+	"github.com/laxeder/go-shop-service/pkg/modules/logger"
+	"github.com/laxeder/go-shop-service/pkg/modules/response"
+)
+
+func ListAccounts(ctx *fiber.Ctx) error {
+
+	var log = logger.New()
+
+	accounts, err := account.Repository().GetList()
+	if err != nil {
+		log.Error().Err(err).Msgf("Erro ao tentar listar contas")
+		return response.Ctx(ctx).Result(response.ErrorDefault("BLC031"))
+	}
+
+	return response.Ctx(ctx).Result(response.Success(200, accounts))
+
+}
