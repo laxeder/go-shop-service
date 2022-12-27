80692d325a47d4fe42a2f32211f84eaac9c767ca
laxeder <laxederbr@gmail.com>
2022-12-27 09:56:16 -0300

    add databases

--- pkg/modules/redisdb/Databases.go
+++ pkg/modules/redisdb/Databases.go
@@ -5,8 +5,11 @@ type Nodedatabase int
 const (
 	Bridge             Nodedatabase = 0
 	UserDatabase       Nodedatabase = 1
+	PermissionDatabase Nodedatabase = 1
 	AddressDatabase    Nodedatabase = 1
 	AccountDatabase    Nodedatabase = 1
 	ProductDatabase    Nodedatabase = 2
 	CategoriesDatabase Nodedatabase = 2
+	ShopCartDatabase   Nodedatabase = 2
+	FreightDatabase    Nodedatabase = 2
 )
