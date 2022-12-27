d03f79fcc60c28679832afc0567e6a1120c388a7
laxeder <laxederbr@gmail.com>
2022-12-27 09:56:53 -0300

    add user claims

--- pkg/shared/Claims.go
+++ pkg/shared/Claims.go
@@ -3,8 +3,23 @@ package shared
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt/v4"
+	j "github.com/laxeder/go-shop-service/pkg/modules/jwt"
+	"github.com/laxeder/go-shop-service/pkg/modules/user"
 )
 
-func Claims(ctx *fiber.Ctx) jwt.MapClaims {
-	return ctx.Locals("user").(*jwt.Token).Claims.(jwt.MapClaims)
+func Claims(ctx *fiber.Ctx) *j.Claims {
+	claims := ctx.Locals("user").(*jwt.Token).Claims.(jwt.MapClaims)
+
+	cl := &j.Claims{}
+
+	return cl.InjectMap(claims)
+}
+
+func UserClaims(ctx *fiber.Ctx) *user.User {
+
+	claims := Claims(ctx)
+
+	u := &user.User{}
+
+	return u.InjectMap(claims.Data)
 }
