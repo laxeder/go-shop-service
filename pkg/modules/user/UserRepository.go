2b5655f7cadd93b78252a206ccc746a2fa67a144
laxeder <laxederbr@gmail.com>
2023-01-03 18:00:34 -0300

    update user repository system

--- pkg/modules/user/UserRepository.go
+++ pkg/modules/user/UserRepository.go
@@ -2,14 +2,12 @@ package user
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"strings"
 
 	"github.com/go-redis/redis/v9"
-	"github.com/laxeder/go-shop-service/pkg/modules/logger"
 	"github.com/laxeder/go-shop-service/pkg/modules/redisdb"
-	"github.com/laxeder/go-shop-service/pkg/modules/str"
+	"github.com/laxeder/go-shop-service/pkg/shared/status"
 )
 
 var redisClient *redis.Client
@@ -18,435 +16,327 @@ func Repository() *User {
 	return &User{}
 }
 
-func MarshalBinary(str []string) (data []byte) {
-	var log = logger.New()
+func (u *User) Save(user *User) (err error) {
 
-	data, err := json.Marshal(str)
+	ctx := context.Background()
+	err = nil
+
+	user.GenerateUuid()
+	user.GenerateFullName()
+	user.GenerateDocument()
+	user.GeneratePassword()
+
+	redisClient, err = redisdb.New(redisdb.UserDatabase)
+
 	if err != nil {
-		log.Error().Err(err).Msgf("Erro ao tranformar array em bytes %s", str)
+		return
+	}
+
+	key := fmt.Sprintf("users:%v", user.Uuid)
+
+	_, err = redisClient.Pipelined(ctx, func(rdb redis.Pipeliner) (err error) {
+		rdb.HSet(ctx, key, "uuid", user.Uuid)
+		rdb.HSet(ctx, key, "full_name", user.Fullname)
+		rdb.HSet(ctx, key, "first_name", user.FirstName)
+		rdb.HSet(ctx, key, "last_name", user.LastName)
+		rdb.HSet(ctx, key, "document", user.Document)
+		rdb.HSet(ctx, key, "email", user.Email)
+		rdb.HSet(ctx, key, "telephone", user.Telephone)
+		rdb.HSet(ctx, key, "password", user.Password)
+		rdb.HSet(ctx, key, "salt", user.Salt)
+
+		redisdb.CreateItemInfo(rdb, ctx, key)
+
+		return
+	})
+
+	if err != nil {
+		return
 	}
 
 	return
 }
 
-func UnmarshalBinary(bff []byte) []string {
-	var log = logger.New()
-
-	data := &[]string{}
-
-	err := json.Unmarshal(bff, data)
-	if err != nil {
-		log.Error().Err(err).Msgf("Erro ao tranformar bytes em array %s", bff)
-	}
-
-	return *data
-}
-
-func (u *User) Save(user *User) (err error) {
-
-	var log = logger.New()
-
-	err = nil
-
-	ctx := context.Background()
-	document := str.DocumentPad(user.Document)
-
-	redisClient, err = redisdb.New(redisdb.UserDatabase)
-	if err != nil {
-		log.Error().Err(err).Msgf("Erro ao acessar banco de dados (%v)", redisdb.UserDatabase)
-		return err
-	}
-
-	key := fmt.Sprintf("users:%v", user.Uuid)
-
-	_, err = redisClient.Pipelined(ctx, func(rdb redis.Pipeliner) error {
-		rdb.HSet(ctx, key, "uuid", user.Uuid)
-		rdb.HSet(ctx, key, "full_name", user.Fullname)
-		rdb.HSet(ctx, key, "first_name", user.FirstName)
-		rdb.HSet(ctx, key, "last_name", user.LastName)
-		rdb.HSet(ctx, key, "document", document)
-		rdb.HSet(ctx, key, "email", user.Email)
-		rdb.HSet(ctx, key, "telephone", user.Telephone)
-		rdb.HSet(ctx, key, "password", user.Password)
-		rdb.HSet(ctx, key, "salt", user.Salt)
-		rdb.HSet(ctx, key, "status", string(user.Status))
-		rdb.HSet(ctx, key, "created_at", user.CreatedAt)
-		rdb.HSet(ctx, key, "updated_at", user.UpdatedAt)
-		return nil
-	})
-
-	if err != nil {
-		log.Error().Err(err).Msgf("Não foi possível inserir o usuário com uuid %v no redis.", document)
-		return err
-	}
-
-	return nil
-}
-
 func (u *User) Update(user *User) (err error) {
 
-	var log = logger.New()
-
+	ctx := context.Background()
 	err = nil
 
-	ctx := context.Background()
-	document := str.DocumentPad(user.Document)
+	user.GenerateFullName()
 
 	redisClient, err = redisdb.New(redisdb.UserDatabase)
+
 	if err != nil {
-		log.Error().Err(err).Msgf("Erro ao acessar banco de dados (%v)", redisdb.UserDatabase)
-		return err
+		return
 	}
 
 	key := fmt.Sprintf("users:%v", user.Uuid)
 
-	_, err = redisClient.Pipelined(ctx, func(rdb redis.Pipeliner) error {
+	_, err = redisClient.Pipelined(ctx, func(rdb redis.Pipeliner) (err error) {
 		rdb.HSet(ctx, key, "full_name", user.Fullname)
 		rdb.HSet(ctx, key, "first_name", user.FirstName)
 		rdb.HSet(ctx, key, "last_name", user.LastName)
 		rdb.HSet(ctx, key, "email", user.Email)
 		rdb.HSet(ctx, key, "telephone", user.Telephone)
-		rdb.HSet(ctx, key, "updated_at", user.UpdatedAt)
-		return nil
+
+		redisdb.UpdateItemInfo(rdb, ctx, key)
+
+		return
 	})
 
 	if err != nil {
-		log.Error().Err(err).Msgf("Não foi possível atualizar o usuário com uuid %v no redis.", document)
-		return err
+		return
 	}
 
-	return nil
+	return
 }
 
 func (u *User) Delete(user *User) (err error) {
 
-	var log = logger.New()
-
+	ctx := context.Background()
 	err = nil
 
-	ctx := context.Background()
-	document := str.DocumentPad(user.Document)
-
 	redisClient, err = redisdb.New(redisdb.UserDatabase)
+
 	if err != nil {
-		log.Error().Err(err).Msgf("Erro ao acessar banco de dados (%v)", redisdb.UserDatabase)
-		return err
+		return
 	}
 
-	user.Status = Disabled
-
 	key := fmt.Sprintf("users:%v", user.Uuid)
-	_, err = redisClient.Pipelined(ctx, func(rdb redis.Pipeliner) error {
-		rdb.HSet(ctx, key, "status", string(user.Status))
-		rdb.HSet(ctx, key, "updated_at", user.UpdatedAt)
-		return nil
+
+	_, err = redisClient.Pipelined(ctx, func(rdb redis.Pipeliner) (err error) {
+		redisdb.UpdateItemStatus(rdb, ctx, key, status.Disabled)
+
+		return
 	})
 
 	if err != nil {
-		log.Error().Err(err).Msgf("Não foi possível deletar o usuário com uuid %v no redis.", document)
-		return err
+		return
 	}
 
-	return nil
+	return
 }
 
 func (u *User) Restore(user *User) (err error) {
 
-	var log = logger.New()
-
+	ctx := context.Background()
 	err = nil
 
-	ctx := context.Background()
-	document := str.DocumentPad(user.Document)
-
 	redisClient, err = redisdb.New(redisdb.UserDatabase)
+
 	if err != nil {
-		log.Error().Err(err).Msgf("Erro ao acessar banco de dados (%v)", redisdb.UserDatabase)
-		return err
+		return
 	}
 
-	user.Status = Enabled
-
 	key := fmt.Sprintf("users:%v", user.Uuid)
-	_, err = redisClient.Pipelined(ctx, func(rdb redis.Pipeliner) error {
-		rdb.HSet(ctx, key, "status", string(user.Status))
-		rdb.HSet(ctx, key, "updated_at", user.UpdatedAt)
-		return nil
+
+	_, err = redisClient.Pipelined(ctx, func(rdb redis.Pipeliner) (err error) {
+		redisdb.UpdateItemStatus(rdb, ctx, key, status.Enabled)
+
+		return
 	})
 
 	if err != nil {
-		log.Error().Err(err).Msgf("Não foi possível retaurar o usuário com uuid %v no redis.", document)
-		return err
+		return
 	}
 
-	return nil
+	return
 }
 
 func (u *User) SavePassowrd(user *User) (err error) {
 
-	var log = logger.New()
-
+	ctx := context.Background()
 	err = nil
 
-	ctx := context.Background()
-	document := str.DocumentPad(user.Document)
+	user.GeneratePassword()
 
 	redisClient, err = redisdb.New(redisdb.UserDatabase)
+
 	if err != nil {
-		log.Error().Err(err).Msgf("Erro ao acessar banco de dados (%v)", redisdb.UserDatabase)
-		return err
+		return
 	}
 
 	key := fmt.Sprintf("users:%v", user.Uuid)
-	_, err = redisClient.Pipelined(ctx, func(rdb redis.Pipeliner) error {
+
+	_, err = redisClient.Pipelined(ctx, func(rdb redis.Pipeliner) (err error) {
 		rdb.HSet(ctx, key, "password", user.Password)
 		rdb.HSet(ctx, key, "salt", user.Salt)
-		rdb.HSet(ctx, key, "updated_at", user.UpdatedAt)
-		return nil
+
+		redisdb.UpdateItemInfo(rdb, ctx, key)
+
+		return
 	})
 
 	if err != nil {
-		log.Error().Err(err).Msgf("Não foi possível atualizar o usuário com uuid %v no redis.", document)
-		return err
+		return
 	}
 
-	return nil
+	return
 }
 
 func (u *User) SaveDocument(user *User) (err error) {
 
-	var log = logger.New()
-
+	ctx := context.Background()
 	err = nil
 
-	ctx := context.Background()
-	document := str.DocumentPad(user.Document)
+	user.GenerateDocument()
 
 	redisClient, err = redisdb.New(redisdb.UserDatabase)
+
 	if err != nil {
-		log.Error().Err(err).Msgf("Erro ao acessar banco de dados (%v)", redisdb.UserDatabase)
-		return err
+		return
 	}
 
 	key := fmt.Sprintf("users:%v", user.Uuid)
-	_, err = redisClient.Pipelined(ctx, func(rdb redis.Pipeliner) error {
-		rdb.HSet(ctx, key, "document", document)
-		rdb.HSet(ctx, key, "updated_at", user.UpdatedAt)
+
+	_, err = redisClient.Pipelined(ctx, func(rdb redis.Pipeliner) (err error) {
+		rdb.HSet(ctx, key, "document", user.Document)
+
+		redisdb.UpdateItemInfo(rdb, ctx, key)
+
 		return err
 	})
 
 	if err != nil {
-		log.Error().Err(err).Msgf("Não foi possível atualizar o usuário com uuid %v no redis.", document)
-		return err
+		return
 	}
 
-	return nil
+	return
 }
 
-func (u *User) GetPasswordByUuid(uuid string) (user *User, err error) {
+func (u *User) GetPassword(uuid string) (user *User, err error) {
 
-	var log = logger.New()
-
+	ctx := context.Background()
 	user = &User{}
 	err = nil
 
-	ctx := context.Background()
-
 	redisClient, err := redisdb.New(redisdb.UserDatabase)
+
 	if err != nil {
-		log.Error().Err(err).Msgf("Erro ao acessar banco de dados (%v)", redisdb.UserDatabase)
-		return nil, err
+		return
 	}
 
 	key := fmt.Sprintf("users:%v", uuid)
-	res := redisClient.HMGet(ctx, key, "uid", "uuid", "password", "salt")
+
+	res := redisClient.HMGet(ctx, key, "uuid", "password", "salt")
+
 	err = res.Err()
+
 	if err != nil {
-		log.Error().Err(err).Msgf("Não foi possível encontrar o usuário com uuid: %v.", uuid)
-		return nil, err
+		return
 	}
 
 	err = res.Scan(user)
+
 	if err != nil {
-		log.Error().Err(err).Msgf("Não foi possível mapear um usuário válido com o uuid %v.", uuid)
-		return nil, err
+		return
 	}
 
-	if user.Status == Disabled {
-		user = &User{Status: Disabled}
-		return user, nil
-	}
-
-	return user, nil
+	return
 }
 
 func (u *User) GetByEmail(email string) (user *User, err error) {
 
-	var log = logger.New()
-
+	user = nil
 	err = nil
 
-	ctx := context.Background()
+	users, er := u.GetList()
 
-	redisClient, err := redisdb.New(redisdb.UserDatabase)
-	if err != nil {
-		log.Error().Err(err).Msgf("Erro ao acessar banco de dados (%v)", redisdb.UserDatabase)
-		return nil, err
+	if er != nil {
+		return user, er
 	}
 
-	iter := redisClient.Scan(ctx, 0, "users:*", 0).Iterator()
-	for iter.Next(ctx) {
-		key := iter.Val()
-
-		res := redisClient.HGetAll(ctx, key)
-
-		err = res.Err()
-
-		if err != nil {
-			continue
+	for _, usr := range users {
+		if usr.Email == email {
+			user = &usr
+			break
 		}
-
-		usr := &User{}
-
-		err = res.Scan(usr)
-		if err != nil {
-			continue
-		}
-
-		if usr.Status == Disabled {
-			continue
-		}
-
-		if usr.Email != email {
-			continue
-		}
-
-		user = usr
 	}
 
-	return user, err
+	return
 }
 
-func (u *User) GetUuid(uuid string) (user *User, err error) {
+func (u *User) Get(uuid string) (user *User, err error) {
 
-	var log = logger.New()
-
-	user = &User{}
+	user = nil
 	err = nil
 
 	ctx := context.Background()
 
 	redisClient, err := redisdb.New(redisdb.UserDatabase)
+
 	if err != nil {
-		log.Error().Err(err).Msgf("Erro ao acessar banco de dados (%v)", redisdb.UserDatabase)
-		return nil, err
+		return
 	}
 
 	key := fmt.Sprintf("users:%v", uuid)
-	res := redisClient.HMGet(ctx, key, "uid", "uuid", "status")
 
-	err = res.Err()
-
-	if err != nil {
-		log.Error().Err(err).Msgf("Não foi possível encontrar o usuário com uuid: %v.", uuid)
-		return u, err
-	}
-
-	err = res.Scan(user)
-	if err != nil {
-		log.Error().Err(err).Msgf("Não foi possível mapear um usuário válido para o uuid %v.", uuid)
-		return nil, err
-	}
-
-	if user.Status == Disabled {
-		user = &User{Status: Disabled}
-		return user, nil
-	}
-
-	return user, nil
-}
-
-func (u *User) GetByUuid(uuid string) (user *User, err error) {
-
-	var log = logger.New()
-
-	user = &User{}
-	err = nil
-
-	ctx := context.Background()
-
-	redisClient, err := redisdb.New(redisdb.UserDatabase)
-	if err != nil {
-		log.Error().Err(err).Msgf("Erro ao acessar banco de dados (%v)", redisdb.UserDatabase)
-		return nil, err
-	}
-
-	key := fmt.Sprintf("users:%v", uuid)
 	res := redisClient.HGetAll(ctx, key)
+
 	err = res.Err()
+
 	if err != nil {
-		log.Error().Err(err).Msgf("Não foi possível encontrar o usuário com uuid: %v.", uuid)
-		return nil, err
+		return
 	}
 
 	err = res.Scan(user)
+
 	if err != nil {
-		log.Error().Err(err).Msgf("Não foi possível mapear um usuário válido para o uuid %v.", uuid)
-		return nil, err
+		return
 	}
 
-	if user.Status == Disabled {
-		user = &User{Status: Disabled}
-		return user, nil
+	userInfo := &redisdb.ItemInfo{}
+
+	err = res.Scan(userInfo)
+
+	if err != nil {
+		return
 	}
 
-	// ? esses campos não podem ficar expostos
+	if userInfo.Status == status.Disabled {
+		return nil, nil
+	}
+
+	//? esses campos não podem ficar expostos
 	user.Password = ""
 	user.ConfirmPassword = ""
 	user.Salt = ""
 
-	return user, nil
+	return
 }
 
 func (u *User) GetList() (users []User, err error) {
 
-	var log = logger.New()
-
+	ctx := context.Background()
 	err = nil
 
-	ctx := context.Background()
-
 	redisClient, err := redisdb.New(redisdb.UserDatabase)
+
 	if err != nil {
-		log.Error().Err(err).Msgf("Erro ao acessar banco de dados (%v)", redisdb.UserDatabase)
-		return nil, err
+		return
 	}
 
 	iter := redisClient.Scan(ctx, 0, "users:*", 0).Iterator()
+
+	err = iter.Err()
+
+	if err != nil {
+		return
+	}
+
 	for iter.Next(ctx) {
-		uuid := strings.Replace(iter.Val(), "users:", "", 2)
-		user, uErr := u.GetByUuid(uuid)
+		user, er := u.Get(strings.Replace(iter.Val(), "users:", "", 2))
 
-		if uErr != nil {
+		if er != nil {
 			continue
 		}
 
-		if user.Status == Disabled {
+		if user == nil {
 			continue
 		}
-
-		user.Password = ""
-		user.ConfirmPassword = ""
-		user.Salt = ""
 
 		users = append(users, *user)
 	}
 
-	err = iter.Err()
-	if err != nil {
-		log.Error().Err(err).Msgf("Não foi possível listar os usuários do banco de dados. %v", err)
-		return nil, err
-	}
-
-	return users, nil
+	return
 }
