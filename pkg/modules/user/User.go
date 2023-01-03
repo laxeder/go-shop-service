12c974d1fae8c7d5ea7ea6e4b100f7ae79e5f54b
laxeder <laxederbr@gmail.com>
2023-01-03 18:00:44 -0300

    update user

--- pkg/modules/user/User.go
+++ pkg/modules/user/User.go
@@ -1,14 +1,12 @@
 package user
 
 import (
-	"encoding/json"
 	"fmt"
 
-	"github.com/laxeder/go-shop-service/pkg/modules/logger"
+	"github.com/google/uuid"
 	"github.com/laxeder/go-shop-service/pkg/modules/str"
+	"github.com/laxeder/go-shop-service/pkg/utils"
 )
-
-var log = logger.New()
 
 type User struct {
 	Uuid            string `json:"uuid,omitempty" redis:"uuid,omitempty"`
@@ -21,109 +19,21 @@ type User struct {
 	Password        string `json:"password,omitempty" redis:"password,omitempty"`
 	ConfirmPassword string `json:"confirm_password,omitempty" redis:"-,omitempty"`
 	Salt            string `json:"salt,omitempty" redis:"salt,omitempty"`
-	Status          Status `json:"status,omitempty" redis:"status,omitempty"`
-	CreatedAt       string `json:"created_at,omitempty" redis:"created_at,omitempty"`
-	UpdatedAt       string `json:"updated_at,omitempty" redis:"updated_at,omitempty"`
 }
 
-func New(userByte ...[]byte) (user *User, err error) {
-	user = &User{}
-	err = nil
-
-	if len(userByte) == 0 {
-		return
-	}
-
-	err = json.Unmarshal(userByte[0], user)
-	if err != nil {
-		log.Error().Err(err).Msgf("O json do usuário está incorreto. %s", userByte[0])
-		return
-	}
-
-	return
+func (u *User) GenerateUuid() {
+	u.Uuid = uuid.New().String()
 }
 
-func (u *User) SetFullname() string {
+func (u *User) GenerateFullName() {
 	u.Fullname = fmt.Sprintf("%v %v", u.FirstName, u.LastName)
-	return u.Fullname
 }
 
-func (u *User) SetDocument(document string) string {
-	u.Document = str.DocumentPad(document)
-	return u.Document
+func (u *User) GenerateDocument() {
+	u.Document = str.DocumentPad(u.Document)
 }
 
-func (u *User) Inject(user *User) *User {
-
-	if user.Uuid != "" {
-		u.Uuid = user.Uuid
-	}
-
-	if user.Fullname != "" {
-		u.Fullname = user.Fullname
-	}
-
-	if user.FirstName != "" {
-		u.FirstName = user.FirstName
-	}
-
-	if user.LastName != "" {
-		u.LastName = user.LastName
-	}
-
-	if user.Document != "" {
-		u.Document = user.Document
-	}
-
-	if user.Email != "" {
-		u.Email = user.Email
-	}
-
-	if user.Telephone != "" {
-		u.Telephone = user.Telephone
-	}
-
-	if user.Password != "" {
-		u.Password = user.Password
-	}
-
-	if user.ConfirmPassword != "" {
-		u.ConfirmPassword = user.ConfirmPassword
-	}
-
-	if user.Salt != "" {
-		u.Salt = user.Salt
-	}
-
-	if user.Status != Undefined {
-		u.Status = user.Status
-	}
-
-	if user.CreatedAt != "" {
-		u.CreatedAt = user.CreatedAt
-	}
-
-	if user.UpdatedAt != "" {
-		u.UpdatedAt = user.UpdatedAt
-	}
-
-	return u
-}
-
-func (u *User) InjectMap(userMap any) *User {
-	var log = logger.New()
-
-	b, err := json.Marshal(userMap)
-	if err != nil {
-		log.Error().Err(err).Msgf("Erro ao transformar user map em byte %s", userMap)
-		return u
-	}
-
-	err = json.Unmarshal(b, &u)
-	if err != nil {
-		log.Error().Err(err).Msgf("Erro ao transformar byte em user map %s", userMap)
-		return u
-	}
-
-	return u
+func (u *User) GeneratePassword() {
+	u.Salt = utils.NewSalt()
+	u.Password = utils.NewHashPassword(u.Salt, u.Password)
 }
